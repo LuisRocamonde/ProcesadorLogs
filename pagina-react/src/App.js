8bfb513dc4c592cceea88f89a9edb8f6652a4d12
Luis Rocamonde <luisrocamonde@gmail.com>
2019-09-22 14:59:06 +0200

    Login y formateado
    Formateada pagina principal, creando login.

--- pagina-react/src/App.js
+++ pagina-react/src/App.js
@@ -7,6 +7,8 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      logeado : false,
+      control : -1,
       error: null,
       logs: []
     };
@@ -38,6 +40,13 @@ class App extends Component {
       return true;
   }
 
+epochToDate(date){
+  var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
+  d.setUTCSeconds(date);
+  return d.toString()
+}
+
+
   //Devuelve un array de elementos con los varoles a mostrar en formato apropiado
   arrayElementos(log){
     var elementos = []
@@ -49,9 +58,11 @@ class App extends Component {
     } else{
       log["value"] = "null"
     }
-      log["_id"]= log["_id"]["$oid"]
+    log["timestamp"] = this.epochToDate(log["timestamp"]);
       for(var key in log){
+        if(key==="elementReference" || key==="elementQualifier" || key==="value" || key==="agent" || key==="timestamp"){
           elementos.push(key + " -> " + log[key])
+        }
       }
 
 
@@ -76,36 +87,98 @@ class App extends Component {
       )
   }
 
+  funcionSubmit(user){
+    console.log(user)
+  /*  if(this.state["control"] > 3){
+      console.log("SUBMIT");
+      this.setState({
+        logeado: true
+      });
+    } else{
+      console.log("CONTROLSUB");
+      console.log(this.state["control"])
+      this.setState({
+        control: this.state["control"] + 1
+      });
+    }*/
+  }
+
+  desloguear(){
+    var control = false;
+    if(control){
+      console.log("FUERA");
+      this.setState({
+        logeado: false
+      });
+    } else{
+      control = true;
+      console.log("CONTROL");
+    }
+
+  }
+
+  obtenerMedico(logs){
+    var medico = "";
+    logs.map(log => (medico = log["agent"]))
+    return medico
+  }
 
   render(){
-    const { error, isLoaded, logs } = this.state;
+    const { error, isLoaded, logs, logeado } = this.state;
     if(error){
       return <div>Error: {error.message}</div>;
     }
     else{
-      if(logs){
-        logs.map(log =>(this.eliminarNulos(log)))
-        return (
-          <div className="App">
-          {logs.map(log => (
-            <ul>
-             <Categoria name={log["timestamp"]} items={this.arrayElementos(log)} icon="cube"/>
+      if(logeado){
+        if(logs){
+          logs.map(log =>(this.eliminarNulos(log)))
+          return (
+
+            <div className="App">
+            <ul className="horizontal">
+
+              <li>{this.obtenerMedico(logs)}</li>
+              <li class="rightli" style={{float:'right'}}>Ultima firma</li>
+              <li><button onClick={this.desloguear()}>FUERA</button></li>
             </ul>
-            ))}
-            <button onClick={this.handleClick.bind(this, JSON.stringify(logs))}>
-            Firmar cambios
-            </button>
-          </div>
+            {logs.map(log => (
+              <ul>
+               <Categoria name={this.epochToDate(log["timestamp"])} items={this.arrayElementos(log)} icon="cube"/>
+              </ul>
+              ))}
+              <button onClick={this.handleClick.bind(this, JSON.stringify(logs))}>
+              Firmar cambios
+              </button>
+            </div>
 
-        );
-      } else if(isLoaded){
+          );
+        } else if(isLoaded){
 
-      }
-      else{
+        }
+        else{
+          return(
+            <h1>FALLO</h1>
+          )
+        }
+      } else{
         return(
-          <h1>FALLO</h1>
-        )
+          <div className="App">
+          <ul className="horizontal">
+            <li>{this.obtenerMedico(logs)}</li>
+            <li className="rightli" style={{float:'right'}}>Ultima firma</li>
+          </ul>
+
+            <form className="formulario" onSubmit={this.funcionSubmit(document.getElementById("user"))}>
+              <label className="labelUsuario">Usuario</label>
+              <input className="textoUsuario" type="text" id="user" name="user" required size="20"/>
+              <label className="labelContraseña">Contraseña</label>
+              <input className="textoContraseña" type="password" id="pass" name="pass" required size="20"/>
+              <input className="submit" type="submit" value="Iniciar Sesion"/>
+            </form>
+          </div>
+        );
       }
+
     }
   }
 }
